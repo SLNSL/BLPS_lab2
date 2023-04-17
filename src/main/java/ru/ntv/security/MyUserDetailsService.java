58423fd305bfa3a5efd399b98bb3444c73fe16d2
SLNSL <nik231002@mail.ru>
2023-04-17 23:06:36 +0300

    downgrade

--- src/main/java/ru/ntv/security/MyUserDetailsService.java
+++ src/main/java/ru/ntv/security/MyUserDetailsService.java
@@ -6,9 +6,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import ru.ntv.entity.User;
-import ru.ntv.repo.UserRepository;
-
+import ru.ntv.entity.users.User;
+import ru.ntv.repo.user.UserRepository;
 import java.util.Collections;
 
 @Service
@@ -16,7 +15,7 @@ public class MyUserDetailsService implements UserDetailsService {
 
     @Autowired
     private UserRepository userRepository;
-
+    
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         final User user = userRepository
