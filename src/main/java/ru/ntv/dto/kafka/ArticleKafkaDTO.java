b1ffbcde41beb3fbc055b1bcc9445d294036bc99
Кошкарбаев Никита <n.koshkarbaev@tinkoff.ru>
2023-12-25 22:13:28 +0300

    add telegram service

---
+++ src/main/java/ru/ntv/dto/kafka/ArticleKafkaDTO.java
@@ -0,0 +1,16 @@
+package ru.ntv.dto.kafka;
+
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Data
+public class ArticleKafkaDTO implements Serializable {
+    private List<ThemeDTO> themes;
+    private String header;
+    private String subheader;
+    private String text;
+    private String photoURL;
+}
